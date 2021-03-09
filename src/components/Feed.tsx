120b6b96a6b41bf50089a71af8f390c3310927fb
川嶋康太 <>
2021-03-10 03:56:36 +0900

    first commit

---
+++ src/components/Feed.tsx
@@ -0,0 +1,65 @@
+import React,{useState,useEffect} from "react";
+import {db} from "../firebase/index"
+import TweetInput from "./TweetInput";
+import styles from "./Feed.module.css"
+import Post from "./Post";
+
+const Feed :React.FC= () => {
+  const [posts,setPosts] = useState([
+    {
+      id:"",
+      avatar:"",
+      image:"",
+      text:"",
+      timestamp:null,
+      username:""
+    }
+  ]);
+
+  //マウント時に一回実行
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      //タイムスタンプの降順で並べ替える
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        )
+      );
+    return () => {
+      unSub();
+    };
+  }, []);
+
+  return(
+    <div className={styles.feed}>
+      <TweetInput />
+      {posts[0]?.id && (
+        <>
+          {/* dbから受け取ったデータをPostコンポーネントを用いて展開 */}
+          {posts.map((post)=>{
+            return <Post 
+            key={post.id}
+            postId={post.id} 
+            avatar={post.avatar} 
+            image={post.image}
+            text={post.text}
+            timestamp={post.timestamp}
+            username={post.username}
+            />
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Feed;
---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAiCYCfpwIIsadNgRN3xFbn3u6RLHgF_oI",
+  authDomain: "reading-recoder.firebaseapp.com",
+  databaseURL: "https://reading-recoder.firebaseio.com",
+  projectId: "reading-recoder",
+  storageBucket: "reading-recoder.appspot.com",
+  messagingSenderId: "177752633329",
+  appId: "1:177752633329:web:9759a577e3e8e91919de50",
+  measurementId: "G-WXMGVJSP95"
+};
+
+export default firebaseConfig;
